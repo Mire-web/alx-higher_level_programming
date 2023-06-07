9b528d7b96ed238964bdc41b85823fccdef406f0
mire-web <amahe8664@gmail.com>
2023-06-07 23:35:56 +0100

    tebahpla

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    if (i >= 97 and i <= 122):
+        if i % 2 == 0:
+            odd = 0
+        else:
+            odd = 32
+        print(chr(i - odd), end="")
