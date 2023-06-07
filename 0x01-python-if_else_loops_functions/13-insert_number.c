9caa2cfaedbf8becbedb52e0b8b4092d8755e198
mire-web <amahe8664@gmail.com>
2023-06-07 20:56:00 +0100

    insert

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -16,32 +16,54 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new_node;
 	int found = 0;
 
-	while (current && current->next)
+	if (check_list(head) == -1)
 	{
-		if (current->n > number)
+		new_node = create_node(new_node, number);
+		*head = new_node;
+	}
+	else
+	{
+		while (current && current->next)
 		{
-			new_node = (listint_t *)malloc(sizeof(listint_t));
+			if (current->n > number)
+			{
+				new_node = create_node(new_node, number);
+				if (!new_node)
+					return (NULL);
+				prev->next = new_node;
+				new_node->next = current;
+				found = 1;
+				break;
+			}
+			prev = current;
+			current = current->next;
+		}
+		if (found == 0)
+		{
+			new_node = create_node(new_node, number);
 			if (!new_node)
 				return (NULL);
-			new_node->n = number;
-			new_node->next = NULL;
+			prev = current;
 			prev->next = new_node;
-			new_node->next = current;
-			found = 1;
-			break;
 		}
-		prev = current;
-		current = current->next;
-	}
-	if (found == 0)
-	{
-		new_node = (listint_t *)malloc(sizeof(listint_t));
-		if (!new_node)
-			return (NULL);
-		new_node->n = number;
-		new_node->next = NULL;
-		prev = current;
-		prev->next = new_node;
 	}
 	return (new_node);
+}
+
+int check_list(listint_t **head)
+{
+	if (!*head)
+		return (-1);
+	else
+		return (0);
+}
+
+listint_t *create_node(listint_t *node, int number)
+{
+	node = (listint_t *) malloc(sizeof(listint_t));
+	if (!node)
+	return (NULL);
+	node->n = number;
+	node->next = NULL;
+	return(node);
 }
