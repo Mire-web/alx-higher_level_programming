1639a518696cfd8476553a1fce900d3e0ea8cd77
mire-web <amahe8664@gmail.com>
2023-06-07 20:27:48 +0100

    insert

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -40,6 +40,7 @@ listint_t *insert_node(listint_t **head, int number)
 			return (NULL);
 		new_node->n = number;
 		new_node->next = NULL;
+		prev = current;
 		prev->next = new_node;
 	}
 	return (new_node);
