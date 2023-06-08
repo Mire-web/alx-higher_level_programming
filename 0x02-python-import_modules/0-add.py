504d30876833ab554b415c369f99384f06d25c2e
mire-web <amahe8664@gmail.com>
2023-06-08 21:06:04 +0100

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-a = 1
-b = 2
-add_num = __import__("add_0")
-print('{:d} + {:d} = {:d}'.format(a, b, add_num.add(a, b)))
+if __name__ = "__main__":
+    a = 1
+    b = 2
+    add_num = __import__("add_0")
+    print('{:d} + {:d} = {:d}'.format(a, b, add_num.add(a, b)))
