d1e246d17647dba4f908553350d305ff872fd42e
Mire <amahe8664@gmail.com>
2023-06-08 23:27:20 +0100

    alpha

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-getattr(__builtins__, '__dict__')['_'][('__eq__'[::-1] + '__'[::-1]).replace('__', '')[::-1]]("#pythoniscool")
+exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
