bac8fb24682c8683e31368da4df41bf3862441c7
mire-web <amahe8664@gmail.com>
2023-06-08 23:58:30 +0100

    omo

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,19 +3,19 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
     argv = sys.argv
-    if len(argv != 3):
+    if len(argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         sys.exit(1)
-    a = argv[1]
-    b = argv[3]
+    a = int(argv[1])
+    b = int(argv[3])
     if argv[2] == '+':
-        print('{:d} + {:d} = {:d}'.format(a, b, add(int(a), int(b))))
+        print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     elif argv[2] == '-':
-        print('{:d} - {:d} = {:d}'.format(a, b, sub(int(a), int(b))))
+        print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     elif argv[2] == '*':
-        print('{:d} * {:d} = {:d}'.format(a, b, mul(int(a), int(b))))
+        print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
     elif argv[2] == '/':
-        print('{:d} / {:d} = {:d}'.format(a, b, div(int(a), int(b))))
+        print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
         sys.exit(1)
