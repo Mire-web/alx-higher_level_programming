586c90fbb134e5400a8bcddbc9a19ce4e73d3e8f
mire-web <amahe8664@gmail.com>
2023-06-25 14:11:51 +0100

    searchnreplace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def find_search(element):
-        return element if element != search else replace
-    return list(map(find_search, my_list))
+    new_list = []
+    for x in my_list:
+        if x == search:
+            x = replace
+        new_list.append(x)
+    return new_list
