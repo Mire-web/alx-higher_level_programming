df150e67a231642fb7938946df279c4ddd0d9000
mire-web <amahe8664@gmail.com>
2023-07-07 21:27:31 +0100

    my_solution

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sortedList = [item for item in a_dictionary]
+    sortedList = [key for key in a_dictionary.keys()]
     sortedList.sort()
     for keys in sortedList:
         print('{}: {}'.format(keys, a_dictionary.get(keys)))
