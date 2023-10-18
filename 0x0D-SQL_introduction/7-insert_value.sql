041f17a18d6952538757da92082952f648847a84
Mire <amahe8664@gmail.com>
2023-10-18 23:43:35 +0100

    6 & 7 mine

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,7 +1,3 @@
--- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
--- New row:
---     id = 89
---     name = Best school
--- The database name will be passed as an argument of the mysql command
+-- Inserts a new row into the database
 
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
+INSERT INTO first_table(id ,name) VALUES(89, 'Best School')
