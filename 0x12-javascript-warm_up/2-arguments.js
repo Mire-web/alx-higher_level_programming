e8501d0ebee8d2c5272b99fb21e340a81bb58d7e
mire-web <amahe8664@gmail.com>
2023-11-14 02:39:44 +0100

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const { argv } = require('node:process');
 
-if (process.argv.length < 3) {
+if (argv.length < 3) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
