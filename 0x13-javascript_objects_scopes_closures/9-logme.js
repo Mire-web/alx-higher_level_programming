32745119ec14a5a259eaab6aecc96fb2868dbee5
mire-web <amahe8664@gmail.com>
2024-02-14 00:20:18 +0100

    100

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let NoPrintedArgument = 0;
 exports.logMe = function (item) {
-  console.log(NoPrintedArgument+': ' + item);
+  console.log(NoPrintedArgument + ': ' + item);
   NoPrintedArgument++;
 };
