713bfa9c876737498c3a287d764cf770884b44d5
mire-web <amahe8664@gmail.com>
2024-02-13 10:33:04 -0800

    2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-11;rgb:0000/0000/0000module.exports = class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0 ) {
       this.width = w;
