2a9052f97ab6fc42300aa2117098d33f0075ed0a
mire-web <amahe8664@gmail.com>
2024-02-14 08:22:22 +0100

    file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs')
-let fileOne = fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    fs.appendFile(process.argv[3], data, (err) => {
-	console.log(err);
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    fs.readFile(process.argv[3], 'utf8', (err, data) => {
+    fs.appendFile(process.argv[4], data, (err) => {
+	if (err) throw err;
     });
+    }
 })
 console.log(fileOne)
