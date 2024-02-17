8cef3f200b4b37dcf9b8e495280feb47f0d55481
mire-web <amahe8664@gmail.com>
2024-02-17 14:24:14 +0100

    0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+AUTHOR: Mire-web
+Desc: Prints all states from hbtn_0e_0_usa
+Date: 17/02/2024
+"""
+import sys
+import MySQLdb
+
+args = sys.argv
+db = MySQLdb.connect(host='localhost',
+                     port=3306,
+                     user=args[1],
+                     passwd=args[2],
+                     db=args[3])
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+for item in cur.fetchall():
+    print(item)
