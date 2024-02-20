69c5feb1fa2fd0328922962490f527b7bc4c073e
mire-web <amahe8664@gmail.com>
2024-02-20 13:28:12 +0100

    7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Author: Mire-web
+Desc: Lists all state object from database
+Date: 20/02/2024
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+args = sys.argv
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(args[1], args[2], args[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_list = session.query(State).order_by(State.id.asc())
