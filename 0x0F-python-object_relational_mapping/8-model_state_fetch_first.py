21e88d331296f0488c16c70fe686d648fb2325de
mire-web <amahe8664@gmail.com>
2024-02-20 18:01:49 +0100

    8-correct

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
     first_state = session.query(State).order_by(State.id.asc()).first()
-    if first_state:
+    if first_state != None:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
         print()
