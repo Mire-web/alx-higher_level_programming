10c5f8e77f1a13481338f120f9989e428e99ed5a
mire-web <amahe8664@gmail.com>
2024-02-22 05:15:52 +0100

    relationships

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Author: Mire-web
+Desc: Definition of State an dInstance of base
+Date: 20/02/2024
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class
+       Columns: Id and name
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
