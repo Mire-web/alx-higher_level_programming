72f72ee56f2d60c6285471a554db1c712899fc2a
mire-web <amahe8664@gmail.com>
2024-06-14 18:18:09 +0100

    async/await

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,34 +1,31 @@
 #!/usr/bin/node
-
+/*
+Script to return all characters in a starwars movie in the order it appeared
+AUTHOR: MIRE-WEB
+*/
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
-let characters = [];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  characters = data.characters;
-  getCharacters(0);
-});
-
-const getCharacters = (index) => {
-  if (index === characters.length) {
-    return;
-  }
-
-  request(characters[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
-      return;
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  (err, res, body) => {
+    if (err) throw err;
+    else {
+      const characters = JSON.parse(body).characters;
+      (async () => {
+        try {
+          for (const character of characters) {
+            const name = await new Promise((resolve, reject) => {
+              request(character, (err, res, body) => {
+                if (err) reject(err);
+                else {
+                  resolve(JSON.parse(body).name);
+                }
+              });
+            });
+            console.log(name);
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      })();
     }
-    const characterData = JSON.parse(body);
-    console.log(characterData.name);
-    getCharacters(index + 1);
   });
-};
